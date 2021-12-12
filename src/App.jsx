abdc5499c0f07af1e6200ad6cd267804aa581384
motngaybinhyen <quantrungthanh73@gmail.com>
2021-12-12 12:50:49 +0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Footer, Header } from "./components";
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
+      {/* <h1>Header</h1> */}
       <Header />
       <Footer />
     </div>
