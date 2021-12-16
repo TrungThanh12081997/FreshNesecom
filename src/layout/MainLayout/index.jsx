3dc300527f33d06e94822cc1e10d25c3b4501544
motngaybinhyen <quantrungthanh73@gmail.com>
2021-12-16 17:01:09 +0700

    co button r nha doc ki css

---
+++ src/layout/MainLayout/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import { Footer, Header } from '../../components'
+import Button from "../../components/Button"
+
+export default function MainLayout(children) {
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
+            <Header />
+            <Outlet />
+            <Button />
+            <Footer />
+        </>
+    )
+}
