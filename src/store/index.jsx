ba5ff4ca3e5b5cc475337ba71d8702e05817d103
motngaybinhyen <quantrungthanh73@gmail.com>
2022-01-13 19:27:07 +0700

    ok

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,17 +1,25 @@
-import { createStore, combineReducers } from "redux"
+import { createStore, combineReducers, applyMiddleware } from "redux"
 import AuthReducer from "./authenReducer"
 import productReducer from "./productReducer"
 import userReducer from "./userReducer"
 import userService from "../services/userService"
-
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from "./rootSaga"
 
 const token = JSON.parse(localStorage.getItem("token"))
+const sagaMiddleware = createSagaMiddleware()
+const reducer = combineReducers({
+    auth: AuthReducer,
+    user: userReducer,
+    product: productReducer,
 
+
+})
 const store = createStore(
-    combineReducers({
-        auth: AuthReducer,
-        user: userReducer,
-        product: productReducer
-    })/// gắn reducer
+    reducer,/// gắn reducer
+    applyMiddleware(sagaMiddleware)
 ) //// tạo store
+sagaMiddleware.run(rootSaga)
+// gắn saga
+
 export default store
